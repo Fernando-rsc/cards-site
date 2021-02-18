2013c9308f33ac8e7bed90be49edba8d567c87bb
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2021-02-18 18:20:28 -0300

    Primeiro commit

---
+++ script/image-script.js
@@ -0,0 +1,36 @@
+
+const cardImage1 = window.document.getElementById('primeira-image').style.backgroundImage = "url(/assets/image-card1.jpg)";
+
+const cardImage2 = window.document.getElementById('segunda-image').style.backgroundImage = "url(/assets/image-card2.jpg)";
+
+const cardImage3 = window.document.getElementById('terceira-image').style.backgroundImage = "url(/assets/image-card3.jpg)";
+
+const cardImage4 = window.document.getElementById('quarta-image').style.backgroundImage = "url(/assets/image-card4.jpg)"
+;
+const cardImage5 = window.document.getElementById('quinta-image').style.backgroundImage = "url(/assets/image-card5.jpg)";
+
+const cardImage6 = window.document.getElementById('sexta-image').style.backgroundImage = "url(/assets/image-card6.jpg)";
+
+const cardImage7 = window.document.getElementById('setima-image').style.backgroundImage = "url(/assets/image-card7.jpg)";
+
+const cardImage8 = window.document.getElementById('oitavo-image').style.backgroundImage = "url(/assets/image-card8.jpg)";
+
+const cardImage9 = window.document.getElementById('nono-image').style.backgroundImage = "url(/assets/image-card9.png)";
+
+const cardImage10 = window.document.getElementById('decima-image').style.backgroundImage = "url(/assets/image-card10.jpg)";
+
+const cardImage11 = window.document.getElementById('decima-primeira-image').style.backgroundImage = "url(/assets/image-card11.jpg)";
+
+const cardImage12 = window.document.getElementById('decima-segunda-image').style.backgroundImage = "url(/assets/image-card12.jpg)";
+
+const cardImage13 = window.document.getElementById('decima-terceira-image').style.backgroundImage = "url(/assets/image-card13.jpg)";
+
+const cardImage14 = window.document.getElementById('decima-quarta-image').style.backgroundImage = "url(/assets/image-card14.jpg)";
+
+const cardImage15 = window.document.getElementById('decima-quinta-image').style.backgroundImage = "url(/assets/image-card15.jpg)";
+
+const cardImage16 = window.document.getElementById('decima-sexta-image').style.backgroundImage = "url(/assets/image-card16.png)";
+
+const cardImage17 = window.document.getElementById('decima-setima-image').style.backgroundImage = "url(/assets/image-card17.jpg)";
+
+const cardImage18 = window.document.getElementById('decima-oitava-image').style.backgroundImage = "url(/assets/image-card18.jpg)";
